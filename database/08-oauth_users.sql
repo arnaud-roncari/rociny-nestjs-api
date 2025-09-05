8a4122dcaeed31e80687b536002a43049f5e67dc
Arnaud Roncari <arnaud.roncari@epitech.eu>
2025-09-05 11:12:24 +0200

    fix schemas

--- database/08-oauth_users.sql
+++ database/08-oauth_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE api.oauth_users (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES api.users(id) ON DELETE CASCADE,
     provider TEXT NOT NULL, -- google, apple
-    provider_user_id TEXT -- id from google, apple
+    provider_user_id TEXT, -- id from google, apple
     access_token TEXT,
-    token_expiration TIMESTAMP,
+    token_expiration TIMESTAMP
 );
